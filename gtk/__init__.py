1a34f8dcbce892759aae4fd89fd0a00b0e6a429f
Jeff <jdost@users.noreply.github.com>
2020-12-29 22:30:28 -0800

    [gtk] Change to Papirus for icons

--- gtk/__init__.py
+++ gtk/__init__.py
@@ -1,5 +1,7 @@
 from cfgtools.files import XDGConfigFile
+from cfgtools.system.arch import Pacman
 
+packages = {Pacman("papirus-icon-theme")}
 files=[
     XDGConfigFile(f"{__name__}/settings.ini", "gtk-3.0/settings.ini"),
 ]

f160fb7e6cd2b0a0d212a31500b9fb9aefaa74c0
Jeff <jdost@users.noreply.github.com>
2021-01-13 20:32:37 -0800

    [auth] Add the auth configs
    - TOTP via ykman
    - Passwords via pass, backed with the repo in dropbox

---
+++ auth/__init__.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+
+from cfgtools.files import EnvironmentFile, XDGConfigFile
+from cfgtools.system.arch import Pacman
+from cfgtools.system import GitRepository
+
+import dropbox
+import rofi
+
+packages = {
+    Pacman("pass"),
+    Pacman("yubikey-manager"),
+    Pacman("xclip"),
+    Pacman("xdotool"),
+    GitRepository(
+        Path.home() / ".local/dropbox/pass.git",
+        Path.home() / ".local/password_store1",
+    ),
+}
+files = [
+    XDGConfigFile(f"{__name__}/totp_rofi", "rofi/scripts/totp"),
+    XDGConfigFile(f"{__name__}/pass_rofi", "rofi/scripts/pass"),
+    XDGConfigFile(f"{__name__}/config.rasi", "rofi/themes/auth.rasi"),
+    EnvironmentFile(__name__),
+]

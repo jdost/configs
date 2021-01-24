924679767c3be51de5d6a71653715f195416ca0f
Jeff <jdost@users.noreply.github.com>
2021-01-23 23:19:15 -0800

    [xorg] Move systemd config down the precedence
    This launches the graphical services, which rely on some configs set in
    other definitions, so they should get launched near the end rather than
    right at the beginning otherwise it becomes impossible to pass some
    system definitions (like HiDPI flags).

--- xorg/__init__.py
+++ xorg/__init__.py
@@ -8,7 +8,7 @@ packages={
 files=[
     File(f"{__name__}/xorg.target", XDG_CONFIG_HOME / "systemd/user/xorg.target"),
     File(f"{__name__}/xinitrc", HOME / ".xinitrc"),
-    XDGConfigFile(f"{__name__}/xinitrc.d/01-systemd"),
+    XDGConfigFile(f"{__name__}/xinitrc.d/90-systemd"),
     XDGConfigFile(f"{__name__}/xinitrc.d/10-xmodmap"),
     XDGConfigFile(f"{__name__}/xinitrc.d/10-xresources"),
     XDGConfigFile(f"{__name__}/caps-ctrl-swap.xmodmap"),

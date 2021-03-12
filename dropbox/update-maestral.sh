0aa7a1cfa168a5d5b9f08d7fdc9aa3f61a6e7fb4
Jeff <jdost@users.noreply.github.com>
2021-03-11 20:18:24 -0800

    [dropbox] Fix pip calls in update script

--- dropbox/update-maestral.sh
+++ dropbox/update-maestral.sh
@@ -6,8 +6,8 @@ VENV=$HOME/.local/maestral
 
 echo "Going to upgrade the maestral pip packages in the virtualenv..."
 
-BEFORE=$($VENV/bin/pip freeze)
-$VENV/bin/pip install --upgrade maestral maestral-qt | grep -v "Requirement already satisfied"
+BEFORE=$($VENV/bin/python -m pip freeze)
+$VENV/bin/python -m pip install --upgrade maestral maestral-qt | grep -v "Requirement already satisfied"
 
-[[ "$BEFORE" == "$($VENV/bin/pip freeze)" ]] && exit 1
+[[ "$BEFORE" == "$($VENV/bin/python -m pip freeze)" ]] && exit 1
 systemctl --user restart dropbox

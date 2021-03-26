770797c90451900b116e2aa3c81bc62f0883e8ac
Jeff <jdost@users.noreply.github.com>
2021-03-25 22:23:33 -0700

    [settitle] bugfix in tmux sessions
    The ANSI code wasn't working, so just calling into tmux itself.  Also
    fixed the bad `/bin/sh` alias and the zsh loading name for term-info

--- homebin/settitle.sh
+++ homebin/settitle.sh
@@ -8,5 +8,5 @@ title=${1:-$(tty | cut -d/ -f3-)}
 if [[ -z "${TMUX:-}" ]]; then
    exec echo -ne "\033]0;$title\007"
 else
-   exec printf "\033k$title\033\\"
+   exec tmux rename-window "$title"
 fi

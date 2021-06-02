49ae8588eece1b7a417e43f1bebf2df681d203dc
Jeff <jdost@users.noreply.github.com>
2021-06-01 19:12:43 -0700

    [zsh] Add some fzf extensions
    - `igrep` is an fzf + rg alias, may want to move this to a script rather
      than shell function...
    - use `fzf` for history lookup

--- zsh/settings/01-aliases.zsh
+++ zsh/settings/01-aliases.zsh
@@ -3,5 +3,24 @@ alias serve="python3 -m http.server"
 
 which term-info &>/dev/null && alias clear="clear;term-info"
 # }}}
+# fzf helpers {{{
+{
+    if ! which fzf &>/dev/null; then
+        return 0
+    fi
+
+    igrep() {
+        INITIAL_QUERY="$@"
+        RG_PREFIX="rg --column --line-number --no-heading --color=always --smart-case "
+        FZF_DEFAULT_COMMAND="$RG_PREFIX '$INITIAL_QUERY'" \
+        fzf --bind "change:reload:$RG_PREFIX {q} || true" \
+            --ansi --disabled --query "$INITIAL_QUERY" \
+            --preview "cat {}" \
+            --height=50% --layout=reverse
+    }
+
+    ## TODO: add `cd` wrapper, figure out interactive mode when no args
+}
+# }}}
 
 # vim: ft=zsh foldmethod=marker
--- zsh/settings/08-keymaps.zsh
+++ zsh/settings/08-keymaps.zsh
@@ -77,3 +77,35 @@ function sudo-command-line () {
 zle -N sudo-command-line
 # Defined shortcut keys: [Esc] [Esc]
 bindkey -M viins "\e\e" sudo-command-line
+
+# utilize fzf if installed
+{
+    if ! which fzf &>/dev/null; then
+        return 0
+    fi
+
+    __fzfcmd() {
+        [ -n "$TMUX_PANE" ] && { [ "${FZF_TMUX:-0}" != 0 ] || [ -n "$FZF_TMUX_OPTS" ]; } &&
+        echo "fzf-tmux ${FZF_TMUX_OPTS:--d${FZF_TMUX_HEIGHT:-40%}} -- " || echo "fzf"
+    }
+
+    fzf-history-widget() {
+        local selected num
+        setopt localoptions noglobsubst noposixbuiltins pipefail no_aliases 2> /dev/null
+        selected=( $(fc -rl 1 | perl -ne 'print if !$seen{(/^\s*[0-9]+\**\s+(.*)/, $1)}++' |
+            FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} $FZF_DEFAULT_OPTS -n2..,.. --tiebreak=index --bind=ctrl-r:toggle-sort,ctrl-z:ignore $FZF_CTRL_R_OPTS --query=${(qqq)LBUFFER} +m" $(__fzfcmd)) )
+        local ret=$?
+        if [ -n "$selected" ]; then
+            num=$selected[1]
+            if [ -n "$num" ]; then
+                zle vi-fetch-history -n $num
+            fi
+        fi
+        zle reset-prompt
+        return $ret
+    }
+
+    zle -N fzf-history-widget
+    bindkey -M viins "^R" fzf-history-widget
+    bindkey -M vicmd "?" fzf-history-widget
+}

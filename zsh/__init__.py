f4982f52ee1221b34b192aa29b9f944251ce887a
Jeff Ostendorf <jeff.ostendorf@discordapp.com>
2021-07-20 04:20:46 +0000

    [zsh] Support sudo based chsh
    On the remote workspaces, the user password isn't known, but you have
    passwordless sudo, so allow setting a flag to run the hook with sudo
    rather than directly as the user.

--- zsh/__init__.py
+++ zsh/__init__.py
@@ -1,3 +1,4 @@
+import getpass
 import os
 import subprocess
 
@@ -7,11 +8,13 @@ from cfgtools.system.nix import NixPkgBin
 from cfgtools.system.ubuntu import Apt
 from cfgtools.hooks import after
 
+_SUDO_CHSH = False
+
 packages = {
     Pacman("zsh"), Pacman("zsh-syntax-highlighting"), Pacman("zsh-completions"),
     AUR("./aur/pkgs/cli-utils"),
     Apt("zsh"), Apt("zsh-common"), Apt("zsh-syntax-highlighting"),
-    NixPkgBin("exa-unstable", "exa"), NixPkgBin("ripgrep", "rg"),
+    NixPkgBin("exa"), NixPkgBin("ripgrep", "rg"),
     NixPkgBin("colordiff"), NixPkgBin("fzf"),
 }
 files = [
@@ -27,4 +30,7 @@ def change_user_shell() -> None:
     if os.environ.get("SHELL") == zsh_bin:
         return
 
-    subprocess.run(["chsh", "-s", zsh_bin])
+    if _SUDO_CHSH:
+        subprocess.run(["sudo", "chsh", "-s", zsh_bin, getpass.getuser()])
+    else:
+        subprocess.run(["chsh", "-s", zsh_bin])

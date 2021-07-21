b339852ece3832e5501fabd70e0ad915dd32a5f3
Jeff <jdost@users.noreply.github.com>
2021-07-20 23:09:04 -0700

    [cal] Add desktop file
    Just launches a terminal with calcurse in it

--- cal/__init__.py
+++ cal/__init__.py
@@ -1,4 +1,4 @@
-from cfgtools.files import XDGConfigFile, XDG_CONFIG_HOME, UserBin
+from cfgtools.files import DesktopEntry, XDGConfigFile, XDG_CONFIG_HOME, UserBin
 from cfgtools.hooks import after
 from cfgtools.system.arch import Pacman
 from cfgtools.system.python import VirtualEnv
@@ -13,6 +13,7 @@ files = [
     UserService(f"{__name__}/gcal-sync.service"),
     UserService(f"{__name__}/gcal-sync.timer"),
     XDGConfigFile(f"{__name__}/config", "calcurse/conf"),
+    DesktopEntry(f"{__name__}/calcurse.desktop"),
     dropbox.EncryptedFile(
         "credentials/gcal-sync.json.gpg",
         XDG_CONFIG_HOME / "calcurse/credentials.json",

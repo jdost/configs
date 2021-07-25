a5c8f7e4aff9ed4fa1d2535c9ce7522d2aaea751
Jeff <jdost@users.noreply.github.com>
2021-07-24 18:55:26 -0700

    [homebin] Add wezterm support to settitle
    Wezterm likes another ANSI code for title setting

--- homebin/settitle.sh
+++ homebin/settitle.sh
@@ -6,7 +6,9 @@ args=$*
 title=${1:-$(tty | cut -d/ -f3-)}
 
 if [[ -z "${TMUX:-}" ]]; then
-   exec echo -ne "\033]0;$title\007"
+    exec echo -ne "\033]0;$title\007"
+elif [[ "${TERM_PROGRAM:-}" == "WezTerm" ]]; then
+    exec echo -ne "\x1b]2;$title\x1b\\"
 else
-   exec tmux rename-window "$title"
+    exec tmux rename-window "$title"
 fi

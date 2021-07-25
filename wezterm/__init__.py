5f215fb858f5e879c4990ce8d9299d9d90786a64
Jeff <jdost@users.noreply.github.com>
2021-07-24 23:21:22 -0700

    [wezterm] Include font AUR package

--- wezterm/__init__.py
+++ wezterm/__init__.py
@@ -1,7 +1,7 @@
-from cfgtools.system.arch import Pacman
+from cfgtools.system.arch import AUR, Pacman
 from cfgtools.files import XDGConfigFile
 
-pkgs = {Pacman("wezterm")}
+pkgs = {Pacman("wezterm"), AUR("./aur/pkgs/ttf-iosevka-ext")}
 files = [
     XDGConfigFile(f"{__name__}/wezterm.lua", "wezterm/wezterm.lua"),
 ]

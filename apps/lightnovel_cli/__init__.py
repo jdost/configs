4a3619e3575c5e198aeb1f4f9179ff6372212b3d
Jeff <jdost@users.noreply.github.com>
2022-07-14 21:27:58 -0700

    [apps] Add lightnovel_cli configs
    CLI webnovel reader, add synced reading list and desktop entry

---
+++ apps/lightnovel_cli/__init__.py
@@ -0,0 +1,16 @@
+import dropbox
+from cfgtools.files import DesktopEntry, XDGConfigFile, normalize
+from cfgtools.system.arch import AUR
+
+NAME = normalize(__name__)
+
+system_packages = {
+    AUR("lightnovel-cli-git")
+}
+files = [
+    XDGConfigFile(
+        dropbox.DROPBOX_DIR / "configs/lightnovel-cli/novels.txt",
+        "lightnovel-cli/novels.txt",
+    ),
+    DesktopEntry(f"{NAME}/lightnovel-cli.desktop"),
+]

45e1c861c0d3fbad7db3399554bbc51995c7bab8
Jeff <jdost@users.noreply.github.com>
2022-07-14 21:28:43 -0700

    [xorg/redshift] Fix service binding target
    The included service uses the `default` target, which launches before
    the display server, so it crashes.  This modifies the after hook to copy
    the service (hardcoded at this time) and modify the target to the custom
    display server one I use and enables it.

--- xorg/redshift.py
+++ xorg/redshift.py
@@ -2,16 +2,31 @@ import dropbox
 from cfgtools.files import XDGConfigFile
 from cfgtools.hooks import after
 from cfgtools.system.arch import Pacman
-from cfgtools.system.systemd import ensure_service
+from cfgtools.system.systemd import UserService, ensure_service
 
-packages={Pacman("redshift")}
-files=[
+packages = {Pacman("redshift")}
+files = [
     XDGConfigFile(
         dropbox.DROPBOX_DIR / "configs/redshift/redshift.conf",
         "redshift/redshift.conf",
     ),
 ]
 
+
 @after
 def enable_redshift():
+    # The service file provided by the package is set to use `default` as the
+    # target, which launches before we have a display manager and crashes, so
+    # we need to make a higher precedence copy (in the ~/.config/system folder)
+    # and fix the launch target
+    # NOTE: users the `UserService` for the path convenience
+    user_service_copy = UserService(
+        "/usr/lib/systemd/user/redshift-gtk.service", "redshift-gtk"
+    )
+    if not user_service_copy.dst.exists():
+        user_service_copy.dst.write_text(
+            user_service_copy.src.read_text().replace(
+                "WantedBy=default.target", "WantedBy=xorg.target"
+            )
+        )
     ensure_service("redshift-gtk", user=True)

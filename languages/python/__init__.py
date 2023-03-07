0a831c35adcc1db6ca17b95d6f514c62963bd3b8
Jeff <jdost@users.noreply.github.com>
2023-03-06 22:04:35 -0800

    [languages/python] Add updater script
    This script will update/upgrade the code tools packages since I don't
    pin them.

--- languages/python/__init__.py
+++ languages/python/__init__.py
@@ -1,4 +1,5 @@
 from cfgtools.files import EnvironmentFile, XDGConfigFile, normalize
+from cfgtools.hooks import after
 from cfgtools.system.arch import Pacman
 from cfgtools.system.python import VirtualEnv
 from cfgtools.system.ubuntu import Apt
@@ -17,13 +18,14 @@ python_tools = [
     "python-lsp-server",
 ]
 
+virtualenv = VirtualEnv("python-code-tools", *python_tools)
 packages = {
     Pacman("python"),
     Pacman("python-wheel"),
     Apt("python3"),
     Apt("python3-pip"),
     Apt("python3-venv"),
-    VirtualEnv("python-code-tools", *python_tools),
+    virtualenv,
 }
 files = [
     XDGConfigFile(f"{NAME}/pip.conf", "pip/pip.conf"),
@@ -32,3 +34,20 @@ files = [
     XDGConfigFile(f"{NAME}/black.toml", "black"),
     XDGConfigFile(f"{NAME}/isort.cfg", "isort.cfg"),
 ]
+
+
+@after
+def create_update_script() -> None:
+    """
+    Write a simple update script that will upgrade the code-tools python
+    packages easily.
+    """
+    update_script = virtualenv.location / "bin/update_pkgs"
+    update_script.write_text(f"""#!/usr/bin/env bash
+
+set -euo pipefail
+
+exec {virtualenv.location / 'bin/python'} -m pip install --upgrade \
+{' '.join(python_tools)} pip
+""")
+    update_script.chmod(0o744)

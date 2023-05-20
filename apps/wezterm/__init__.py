b0efe5ecb29e236f2d080229c8755536ceb71285
Jeff <jdost@users.noreply.github.com>
2023-05-20 10:17:15 -0700

    [apps/wezterm] Support rofi properly
    Initial pass at providing some override for rofi with wezterm as the
    main terminal using the new modular config system.

--- apps/wezterm/__init__.py
+++ apps/wezterm/__init__.py
@@ -1,9 +1,12 @@
 from cfgtools.system.arch import AUR, Pacman
 from cfgtools.files import XDGConfigFile, normalize
 
+import rofi
+
 NAME = normalize(__name__)
 
 pkgs = {Pacman("wezterm"), AUR("./aur/pkgs/ttf-iosevka-ext")}
 files = [
     XDGConfigFile(f"{NAME}/wezterm.lua", "wezterm/wezterm.lua"),
+    rofi.RofiModule(f"{NAME}/rofi.rasi", "wezterm"),
 ]

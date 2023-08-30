388b93482c00b7425a8cc9656c2e52636716d326
Jeff <jdost@users.noreply.github.com>
2023-08-29 18:19:14 -0700

    [streamlink/twitch] Add twitch tracking and streaming configs
    - Add twitch-indicator service for notifications
    - Configure to use web-xdg-open routing
    - Update notifications to have right settings
    - Custom version of
      https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=twitch-indicator
      w/ fixed deps

---
+++ streamlink/twitch/__init__.py
@@ -0,0 +1,28 @@
+import dropbox
+from cfgtools.files import XDG_CONFIG_HOME, normalize
+from cfgtools.hooks import after
+from cfgtools.system.arch import AUR
+from cfgtools.system.systemd import UserService, ensure_service
+from cfgtools.utils import hide_xdg_entry
+
+NAME = normalize(__name__)
+
+packages = {AUR("./aur/pkgs/twitch-indicator")}
+files = [
+    dropbox.EncryptedFile(
+        "credentials/twitch.authtoken.gpg",
+        XDG_CONFIG_HOME / "twitch-indicator/authtoken",
+    ),
+    UserService(f"{NAME}/twitch-indicator.service"),
+]
+
+
+@after
+def enable_twitch_service() -> None:
+    ensure_service("twitch-indicator", user=True)
+
+
+@after
+def hide_unwanted_xdg_entries() -> None:
+    hide_xdg_entry("twitch-indicator")
+    hide_xdg_entry("twitch-indicator-auth")

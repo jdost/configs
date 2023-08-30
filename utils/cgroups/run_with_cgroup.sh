558446bf044aca54d5bd35ce9d3aa32a3f0bd256
Jeff <jdost@users.noreply.github.com>
2023-08-29 17:00:06 -0700

    [utils/cgroups] Cleanup script

--- utils/cgroups/run_with_cgroup.sh
+++ utils/cgroups/run_with_cgroup.sh
@@ -9,18 +9,36 @@ if [[ -z "${1:-}" ]]; then
     exit 1
 fi
 
-CMD=$1
-# This is the lookup file for what cgroup to use for $CMD
-SLICE_MAP=${XDG_CONFIG_HOME:-$HOME/.config}/systemd/slice_map
-# Determine the target cgroup/slice
-SLICE=""
-if [[ -e "$SLICE_MAP" ]]; then
-    if grep "^$CMD" $SLICE_MAP >&/dev/null; then
-        SLICE=$(grep "^$CMD" $SLICE_MAP | cut -d' ' -f2)
-    else
-        echo "$CMD" >> $HOME/tmp/cgroups.log
+lookup_cgroup() {
+    if [[ ! -z "${CGROUP:-}" ]]; then
+        echo "${CGROUP}"
+        return 0
     fi
+
+    local cmd=$1
+    # This is the lookup file for what cgroup to use for $CMD
+    local map=${XDG_CONFIG_HOME:-$HOME/.config}/systemd/slice_map
+    # Determine the target cgroup/slice
+    local slice=""
+    if [[ -e "$map" ]]; then
+        if grep "^$cmd " $map >&/dev/null; then
+            echo $(grep "^$cmd " $map | cut -d' ' -f2)
+            return 0
+        else
+            echo "$cmd" >> $HOME/tmp/cgroups.log
+        fi
+    fi
+    echo ""
+}
+
+CMD=$1
+SLICE=$(lookup_cgroup $CMD)
+
+# If the computed slice is "none" don't run with systemd-run
+if [[ "$SLICE" == "none" ]]; then
+    exec "$@"
 fi
+
 # Now run the defined command with the determined cgroup
 # NOTE: if it's still an empty string, it won't get a cgroup, but will run in an
 #   adhoc service scope that you can apply properties to after the launch

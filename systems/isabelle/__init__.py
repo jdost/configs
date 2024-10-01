4118ba9217525cff02c8af2f6b688e3edf3bd345
Jeff <jdost@users.noreply.github.com>
2024-10-01 15:48:57 -0700

    [systems] Correct isabelle config
    The others need to be updated but they are littered with other stuff
    that needs to be cleaned up.

--- systems/isabelle/__init__.py
+++ systems/isabelle/__init__.py
@@ -4,12 +4,11 @@ import auth
 import bat
 import git
 import gpg
-import networkmanager
 import python
-import screenlock
 import shells.zsh
 import utils.docker
 import utils.dropbox
+import utils.networkmanager
 import utils.ssh
 import utils.tmux
 import utils.unclutter
@@ -17,8 +16,9 @@ import utils.user_dirs
 import utils.wallpaper
 import vim
 import xorg.autorandr
-import xorg.icons.papirus
+import utils.icons.papirus
 import xorg.polybar
+import xorg.screenlock
 import xorg.window_managers.bspwm
 from cfgtools.files import HOME, File, XinitRC
 from cfgtools.systems import set_default_shell

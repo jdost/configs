516d883c1d714c290ccb46de9e1ea669036e2284
Jeff <jdost@users.noreply.github.com>
2024-10-02 10:45:23 -0700

    [vim] Fix some initial setup bits for nvim
    - Smarter sourcing for neovim plugins
    - Ensure the pynvim is present
    - Switch apt installs to nix (dunno how this will look long term)

--- vim/__init__.py
+++ vim/__init__.py
@@ -1,13 +1,17 @@
 from cfgtools.files import EnvironmentFile, UserBin, XDGConfigFile, normalize
 from cfgtools.system.arch import Pacman
-from cfgtools.system.ubuntu import Apt
+from cfgtools.system.nix import NixPkgBin
 
 NAME = normalize(__name__)
 CONFIG_FILES = ["vimrc", "ftplugin", "plugin", "plugins.vim"]
 
-packages={Pacman("vim"), Apt("vim")}
+packages={
+    Pacman("neovim"), Pacman("python-pynvim"),
+    NixPkgBin("neovim"),
+}
 files=[
     EnvironmentFile(NAME),
     UserBin(f"{NAME}/wrapper.sh", "vim"),
+    XDGConfigFile("vim/neovim.vim", "nvim/init.vim"),
 ] + [XDGConfigFile(f"{NAME}/{f}") for f in CONFIG_FILES]
 
--- vim/plugins.vim
+++ vim/plugins.vim
@@ -1,6 +1,6 @@
-if empty(glob('$XDG_CONFIG_HOME/vim/autoload/plug.vim'))
-  silent !curl -fLo "$XDG_CONFIG_HOME/vim/autoload/plug.vim" --create-dirs
-    \ https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+let data_dir = has('nvim') ? stdpath('data') . '/site' : '$XDG_CONFIG_HOME/vim'
+if empty(glob(data_dir . '/autoload/plug.vim'))
+  silent execute '!curl -fLo '.data_dir.'/autoload/plug.vim" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
   autocmd VimEnter * PlugInstall --sync | source $MYVIMRC
 endif
 

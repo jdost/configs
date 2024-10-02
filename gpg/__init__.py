bae5d7154082a5b14548513d03ed30a366f3551d
Jeff <jdost@users.noreply.github.com>
2024-10-02 10:39:59 -0700

    [gpg] Add wayland support
    Right now this is mashed together with x, probably need to figure out
    splitting better.

--- gpg/__init__.py
+++ gpg/__init__.py
@@ -2,16 +2,18 @@ from cfgtools.files import EnvironmentFile, File, Folder, HOME, XinitRC, normali
 from cfgtools.hooks import after
 from cfgtools.system.arch import Pacman
 from cfgtools.system.systemd import ensure_service
+from wayland import WaylandRC
 
 NAME = normalize(__name__)
 USER_GPG_FOLDER = HOME / ".local/gpg"
 GPG_CONFIG_FILES = ["gpg.conf", "gpg-agent.conf", "scdaemon.conf"]
 
-packages={Pacman("gnupg"), Pacman("pcsclite"), Pacman("ccid")}
+packages={Pacman("gnupg"), Pacman("pcsclite"), Pacman("ccid"), Pacman("gcr")}
 files=[
     EnvironmentFile(NAME),
     Folder(USER_GPG_FOLDER, permissions=0o700),
     XinitRC(NAME, priority=20),
+    WaylandRC(f"{NAME}/waylandrc", "gpg", priority=20),
 ] + [File(f"{NAME}/{f}", USER_GPG_FOLDER / f) for f in GPG_CONFIG_FILES]
 
 

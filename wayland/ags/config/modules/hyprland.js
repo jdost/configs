bfca2b2b35a07e6e76fef1c625d4c9ba4d80b08b
Jeff <jdost@users.noreply.github.com>
2024-12-19 17:39:25 -0800

    [wayland/ags] Fix workspace ordering
    Sorts the workspace buttons to be by id so when dynamic workspaces get
    added, they show up in the right spot in the widget.

--- wayland/ags/config/modules/hyprland.js
+++ wayland/ags/config/modules/hyprland.js
@@ -19,44 +19,48 @@ add_left(
     const activeId = hyprland.active.workspace.bind("id");
     var monitorID = "";
     const workspaces = hyprland.bind("workspaces").as((workspaces) =>
-      workspaces.map(function (workspace, index) {
-        const newMonitor =
-          index > 0 ? monitorID !== workspace.monitorID : false;
-        monitorID = workspace.monitorID;
-        if (workspace.id < 0) {
-          return;
-        }
-        var button = Widget.Button({
-          on_primary_click: () =>
-            hyprland.messageAsync(`dispatch workspace ${workspace.id}`),
-          cursor: "pointer",
-          child: Widget.Label(name_icons[workspace.name] || default_icon),
-          class_names: ["ws-button", `monitor-${workspace.monitorID}`],
-          css: newMonitor ? "margin-left: 12px" : "",
-          attribute: workspace.id,
-          setup: function (self) {
-            self.hook(hyprland, function () {
-              var visible = false;
-              hyprland.monitors.forEach(function (monitor) {
-                if (monitor.activeWorkspace.id === workspace.id) {
-                  self.toggleClassName("focused", monitor.focused);
-                  self.toggleClassName("visible", !monitor.focused);
-                  visible = true;
+      workspaces
+        .toSorted(function (a, b) {
+          return a.monitorID - b.monitorID;
+        })
+        .map(function (workspace, index) {
+          const newMonitor =
+            index > 0 ? monitorID !== workspace.monitorID : false;
+          monitorID = workspace.monitorID;
+          if (workspace.id < 0) {
+            return;
+          }
+          var button = Widget.Button({
+            on_primary_click: () =>
+              hyprland.messageAsync(`dispatch workspace ${workspace.id}`),
+            cursor: "pointer",
+            child: Widget.Label(name_icons[workspace.name] || default_icon),
+            class_names: ["ws-button", `monitor-${workspace.monitorID}`],
+            css: newMonitor ? "margin-left: 12px" : "",
+            attribute: workspace.id,
+            setup: function (self) {
+              self.hook(hyprland, function () {
+                var visible = false;
+                hyprland.monitors.forEach(function (monitor) {
+                  if (monitor.activeWorkspace.id === workspace.id) {
+                    self.toggleClassName("focused", monitor.focused);
+                    self.toggleClassName("visible", !monitor.focused);
+                    visible = true;
+                  }
+                });
+                if (!visible) {
+                  self.toggleClassName("focused", false);
+                  self.toggleClassName("visible", false);
                 }
+                self.toggleClassName(
+                  "occupied",
+                  hyprland.getWorkspace(workspace.id)?.windows > 0,
+                );
               });
-              if (!visible) {
-                self.toggleClassName("focused", false);
-                self.toggleClassName("visible", false);
-              }
-              self.toggleClassName(
-                "occupied",
-                hyprland.getWorkspace(workspace.id)?.windows > 0,
-              );
-            });
-          },
-        });
-        return button;
-      }),
+            },
+          });
+          return button;
+        }),
     );
 
     return Widget.Box({

61a4db159af4c433ff11b077feab5660a2ac4e82
Jeff <jdost@users.noreply.github.com>
2024-12-20 13:03:29 -0800

    [ags] Conditional requirements based on modules
    Adds a before-hook to conditionally add dependencies for specific
    modules.  This is currently just `gvfs` which is needed for the mpris
    module (but no others), probably need to move some others.

--- wayland/ags/__init__.py
+++ wayland/ags/__init__.py
@@ -1,16 +1,21 @@
+from typing import Optional
+
 from cfgtools.files import File, InputType, XDGConfigFile, normalize
-from cfgtools.hooks import after
+from cfgtools.hooks import after, before
 from cfgtools.system.arch import AUR, Pacman
 from cfgtools.system.systemd import UserService, ensure_service
-
 from wayland.hyprland import HyprlandSettings
 
 NAME = normalize(__name__)
 
 
 class AgsSettings(File):
+    definition: Optional['AgsSettings'] = None
+    loc = XDGConfigFile.DIR / "ags/settings.json"
+
     def __init__(self, src: InputType):
-        super().__init__(src=src, dst=XDGConfigFile.DIR / "ags/settings.json")
+        AgsSettings.definition = self
+        super().__init__(src=src, dst=self.loc)
 
 
 packages = {
@@ -23,6 +28,28 @@ files = {
     HyprlandSettings(f"{NAME}/hyprland.conf", "ags"),
 }
 
+
+@before
+def conditional_deps() -> None:
+    """There are some dependencies for specific widgets, so lookup the defined
+    widgets and conditionally add them, looks at either an already defined
+    settings file or the one about to be linked.
+    """
+    import json
+
+
+    widgets = set()
+    if AgsSettings.loc.exists():
+        widgets = set(
+            json.loads(AgsSettings.loc.read_text()).get("widgets", [])
+        )
+    elif AgsSettings.definition is not None:
+        widgets = set(
+            json.loads(AgsSettings.definition.src.read_text()).get("widgets", [])
+        )
+
+    if "mpris" in widgets:
+        packages.add(Pacman("gvfs"))
 
 @after
 def enable_ags() -> None:

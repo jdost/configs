8a738149cbc0e89b7d90c2dddffbb064162b0ff3
Jeff <jdost@users.noreply.github.com>
2025-01-15 13:51:07 -0800

    [wayland/hyprland] Sidebar tweaks
    - Make the sidebars bigger, easier to use/read at a third in size
    - Fixed some dispatch logic in helper script to batch
    - Re-shift the window on toggle (to try and keep it in the location)

--- wayland/hyprland/toggle-sidebar.sh
+++ wayland/hyprland/toggle-sidebar.sh
@@ -9,7 +9,7 @@ CLASS="sidebar.$side"
 if ! hyprctl clients | grep "initialClass: $CLASS" &>/dev/null; then
     x="0"
     if [[ "$side" == "right" ]]; then
-        x="80%"
+        x="66%"
     fi
     # there is not an existing window, so spawn it
     exec hyprctl dispatch exec "[move $x 0] wezterm --config window_background_opacity=0.4 start --cwd $HOME --class \"$CLASS\""
@@ -24,10 +24,16 @@ current_workspace=$(hyprctl activeworkspace -j | jq ".id")
 if [[ "$loc" == "$current_workspace" ]]; then
     # window is currently on the active workspace, so hide it by sending it to a
     #   special workspace
-    hyprctl dispatch togglespecialworkspace sidebars
-    hyprctl dispatch movetoworkspace "special:sidebars","class:$CLASS"
-    hyprctl dispatch togglespecialworkspace sidebars
+    hyprctl --batch "dispatch togglespecialworkspace sidebars ;
+        dispatch movetoworkspace special:sidebars,class:$CLASS ;
+        dispatch togglespecialworkspace sidebars ;
+        dispatch cyclenext"
 else
     # window is located on another workspace, bring it to this one
     hyprctl dispatch movetoworkspace $current_workspace,"class:$CLASS"
+    if [[ "$side" == "right" ]]; then
+        hyprctl dispatch movewindow r
+    else
+        hyprctl dispatch movewindow l
+    fi
 fi

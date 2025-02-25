d5820b0aa99da5e32dffd3d17a97a8d0fd893e77
Jeff <jdost@users.noreply.github.com>
2025-02-24 20:26:06 -0800

    [gpg] Ensure public key is imported
    This is a common setup step, so just ensure my public key is imported.
    Tried to mark it's trust level but the command won't let python interact
    with it.  Option would be to manipulate the trust db, but I don't want
    to go down that rabbit hole.

--- gpg/__init__.py
+++ gpg/__init__.py
@@ -1,4 +1,9 @@
-from cfgtools.files import EnvironmentFile, File, Folder, HOME, XinitRC, normalize
+import urllib.request
+from pathlib import Path
+
+from cfgtools import utils
+from cfgtools.files import (HOME, EnvironmentFile, File, Folder, XinitRC,
+                            normalize)
 from cfgtools.hooks import after
 from cfgtools.system.arch import Pacman
 from cfgtools.system.systemd import ensure_service
@@ -7,6 +12,9 @@ from wayland import WaylandRC
 NAME = normalize(__name__)
 USER_GPG_FOLDER = HOME / ".local/gpg"
 GPG_CONFIG_FILES = ["gpg.conf", "gpg-agent.conf", "scdaemon.conf"]
+# This is a sharable public link via dropbox
+KEYID = "061ABE27128D52FCBAF4A3F7570D47FF2073CDA5"
+PUBKEY_URL = "https://www.dropbox.com/scl/fi/prcdjw7cj775rmsf1tftx/pubkey.gpg?rlkey=r939kpxou6pszn4tmvti42elx&st=wyivlveq&dl=0"
 
 packages={Pacman("gnupg"), Pacman("pcsclite"), Pacman("ccid"), Pacman("gcr")}
 files=[
@@ -20,3 +28,17 @@ files=[
 @after
 def smartcard_daemon_running() -> None:
     ensure_service("pcscd.socket")
+
+
+@after
+def import_pubkey() -> None:
+    if utils.run(f"gpg --list-keys {KEYID}"):
+        return
+
+    tmp_key = Path(USER_GPG_FOLDER) / "pubkey"
+    with urllib.request.urlopen(PUBKEY_URL) as req:
+        tmp_key.write_bytes(req.read())
+
+    utils.run(f"gpg --import {tmp_key}")
+    # Tried to auto do this, gpg doesn't want programmatic interfacing
+    print(f"Please run `gpg --edit-key {KEYID}` and mark the key as trusted.")

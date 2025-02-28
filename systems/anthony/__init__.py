e1c450553b1f63baca041e5fdd450b1290824611
Jeff <jdost@users.noreply.github.com>
2025-02-27 22:29:14 -0800

    [systems/anthony] Updated config
    This is the *heavily* re-done config for my laptop after a fresh install
    using wayland as the main display system.  Tried to do a number of
    things with this one:
    - More stuff installed by hand is explicitly done via the config
    - Utilizing keyd for the built-in keyboard and trying to use it more

--- systems/anthony/__init__.py
+++ systems/anthony/__init__.py
@@ -1,40 +1,77 @@
-import apps.alacritty
-import apps.streamlink
+import apps.mpv
+import apps.obsidian
+import apps.wezterm
 import apps.zathura
 import aur
+import auth.wayland
+import browsers.firefox
 import browsers.qutebrowser
+import browsers.web_xdg_open as web_xdg_open
 import git
 import gpg
-import gtk
 import languages.python
+import rofi
+import rofi.wayland
 import shells.zsh
-import utils.bat
 import utils.cal
+import utils.cgroups
 import utils.docker
 import utils.dropbox.wayland
 import utils.icons.papirus
-import utils.opensnitch
+import utils.keyd as keyd
+import utils.kubernetes
 import utils.screenshot.wayland
 import utils.ssh
 import utils.tmux
-import utils.unclutter
 import utils.user_dirs
 import utils.wallpaper
 import vim
-import xorg.autorandr
-import xorg.polybar
-import xorg.screenlock
-import xorg.window_managers.bspwm
-from cfgtools.files import HOME, XDG_CONFIG_HOME, File, XinitRC
+import wayland.ags as ags
+import wayland.gammastep
+import wayland.hyprland as hyprland
+import wayland.hyprland.hyprpaper
+import wayland.screenlock
+from cfgtools.hooks import after
 from cfgtools.system import set_default_shell
+from cfgtools.system.arch import Pacman
+from cfgtools.system.systemd import ensure_service
+from cfgtools.utils import hide_xdg_entry
 
-utils.cal.ENABLED = False
-xorg.polybar.set_icon_font("fluent")
 set_default_shell(shells.zsh.BIN)
+web_xdg_open.set_default("qutebrowser")
 
-File("systems/anthony/polybar", XDG_CONFIG_HOME / "polybar/system")
-File("systems/anthony/Xresources", XDG_CONFIG_HOME / "xorg/Xresources.system")
-File("systems/anthony/bspwm_external_rules.sh", XDG_CONFIG_HOME / "bspwm/external_rules")
-File("systems/anthony/chromium-flags.conf", XDG_CONFIG_HOME / "chromium-flags.conf")
-XinitRC("systems/anthony", priority=40)
-File("systems/anthony/drirc", HOME / ".drirc")
+pkgs = {
+    Pacman("iwd"), Pacman("networkmanager"),  # Networking
+    Pacman("gnome-bluetooth-3.0"), Pacman("bluez-utils"),  # Bluetooth
+    Pacman("brightnessctl"), Pacman("pulsemixer"),  # Media Key helpers
+}
+
+configs = {
+    hyprland.HyprlandSettings("systems/anthony/hyprland.conf", "system", priority=90),
+    ags.AgsSettings("systems/anthony/ags_settings.json"),
+    keyd.KeydConfig("systems/anthony/keyd.conf", "main"),
+    rofi.RofiModule("systems/anthony/rofi.rasi", "system"),
+}
+
+#File("systems/anthony/polybar", XDG_CONFIG_HOME / "polybar/system")
+#File("systems/anthony/chromium-flags.conf", XDG_CONFIG_HOME / "chromium-flags.conf")
+#XinitRC("systems/anthony", priority=40)
+#File("systems/anthony/drirc", HOME / ".drirc")
+
+@after
+def hide_unwanted_xdg_entries() -> None:
+    """Try and move these as close to the source as possible, but sometimes it's
+    tough to keep up when things shift."""
+    for entry in [
+        "avahi-discover", "bssh", "bvnc", # avahi, this gets added by few things
+        "electron32", # electron, downtree from webcord
+    ]:
+        hide_xdg_entry(entry)
+
+@after
+def enable_services() -> None:
+    """Enable services installed in here."""
+    ensure_service("bluetooth")
+    ensure_service("NetworkManager")
+    ensure_service("iwd")
+    ensure_service("pipewire-pulse.socket", user=True)

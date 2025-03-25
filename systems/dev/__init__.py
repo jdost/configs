f5d4108d1860dc4743fb0e606f17265fef153bc5
Jeff <jeff.ostendorf@whatnot.com>
2025-03-25 21:20:42 +0000

    [systems/dev] Add basic development config
    This is kind of a generic config declaration that gets used on
    development systems (usually things like a container or VM) that I end
    up using for work stuff where I want to be able to image and re-build
    the environment.

---
+++ systems/dev/__init__.py
@@ -0,0 +1,15 @@
+import git
+import languages.golang
+import languages.nodejs
+import languages.python
+import languages.rust
+import shells.zsh
+import utils.homebin
+import utils.kubernetes
+import utils.ssh
+import utils.tmux
+import vim
+
+from cfgtools.system import set_default_shell
+
+set_default_shell(shells.zsh.BIN)

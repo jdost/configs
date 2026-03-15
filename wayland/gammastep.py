f05fe4f3aa94d5fe4d28415ae117cf425f2c39d9
Jeff <jdost@users.noreply.github.com>
2026-03-15 14:48:27 -0700

    [wayland/gammastep] XDG entry cleanup

--- wayland/gammastep.py
+++ wayland/gammastep.py
@@ -1,11 +1,15 @@
 from cfgtools.files import XDGConfigFile
+from cfgtools.hooks import after
 from cfgtools.system.arch import Pacman
 from cfgtools.system.systemd import UserService
+from cfgtools.utils import hide_xdg_entry
 from utils import dropbox
 
 packages = {
     Pacman("gammastep"),
-    Pacman("libappindicator-gtk3"), Pacman("gtk3"), Pacman("python-gobject"),  # For systray icon
+    Pacman("libappindicator-gtk3"),
+    Pacman("gtk3"),
+    Pacman("python-gobject"),  # For systray icon
 }
 files = [
     XDGConfigFile(
@@ -15,6 +19,11 @@ files = [
     # Enable the gammastep-indicator to the user wayland target
     UserService(
         "/usr/lib/systemd/user/gammastep-indicator.service",
-        "wayland.target.wants/gammastep-indicator.service"
-    )
+        "wayland.target.wants/gammastep-indicator.service",
+    ),
 ]
+
+
+@after
+def hide_unwanted_xdg_entries() -> None:
+    hide_xdg_entry("gammastep-indicator")

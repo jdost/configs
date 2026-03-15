b2d924e099cd09f00cd70bcaba0d64f579e53879
Jeff <jdost@users.noreply.github.com>
2026-03-15 14:49:48 -0700

    [wayland/hyprland] Cleanup stuff
    - Removed missing package
    - Formatter
    - Fix typo in qutebrowser class

--- wayland/hyprland/__init__.py
+++ wayland/hyprland/__init__.py
@@ -12,11 +12,14 @@ class HyprlandSettings(File):
     config_dir: Path = XDGConfigFile.DIR / "hypr/hyprland.d"
 
     def __init__(self, src: InputType, name: str, priority: int = 50):
-        super().__init__(src=src, dst=HyprlandSettings.config_dir / f"{priority}-{name}.conf")
+        super().__init__(
+            src=src, dst=HyprlandSettings.config_dir / f"{priority}-{name}.conf"
+        )
 
 
 packages = {
-    Pacman("hyprland"), Pacman("xdg-desktop-portal-hyprland"), Pacman("xwaylandvideobridge"),
+    Pacman("hyprland"),
+    Pacman("xdg-desktop-portal-hyprland"),
 }
 files = {
     XDGConfigFile(f"{NAME}/hyprland.conf", "hypr/hyprland.conf"),

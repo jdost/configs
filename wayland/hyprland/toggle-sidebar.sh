a5d36947a467705798823c4cbb07883d7a7fb002
Jeff <jdost@users.noreply.github.com>
2026-03-15 14:50:28 -0700

    [wayland/hyprland] Fix sidebar script
    Big update for the sidebar toggling script to better handle window
    sizing and formatting.

--- wayland/hyprland/toggle-sidebar.sh
+++ wayland/hyprland/toggle-sidebar.sh
@@ -6,13 +6,34 @@ side=${1:-"right"}
 CLASS="sidebar.$side"
 
 
+setup_window() {
+    local current_workspace_info=$(hyprctl monitors -j | jq ".[] | select(.focused)")
+    local target_height=$(echo $current_workspace_info | jq ".height")
+    local target_width=$(echo $current_workspace_info | jq ".width")
+
+    hyprctl dispatch resizewindowpixel exact \
+        $(( $target_width / 3 )) \
+        $(( $target_height )),class:$CLASS
+
+    if [[ "$side" == "right" ]]; then
+        hyprctl dispatch movewindowpixel exact \
+            $(( $target_width * 2 / 3 )) 0,class:$CLASS
+    else
+        hyprctl dispatch movewindowpixel \
+            0 0,class:$CLASS
+    fi
+}
+
 if ! hyprctl clients | grep "initialClass: $CLASS" &>/dev/null; then
     x="0"
     if [[ "$side" == "right" ]]; then
         x="66%"
     fi
     # there is not an existing window, so spawn it
-    exec hyprctl dispatch exec "[move $x 0] wezterm --config window_background_opacity=0.4 start --cwd $HOME --class \"$CLASS\""
+    hyprctl dispatch exec "[move $x 0] wezterm --config window_background_opacity=0.4 start --cwd $HOME --class \"$CLASS\""
+    sleep 0.1
+    setup_window
+    exit 0
 fi
 
 loc=$(
@@ -22,18 +43,18 @@ loc=$(
 current_workspace=$(hyprctl activeworkspace -j | jq ".id")
 
 if [[ "$loc" == "$current_workspace" ]]; then
-    # window is currently on the active workspace, so hide it by sending it to a
-    #   special workspace
-    hyprctl --batch "dispatch togglespecialworkspace sidebars ;
-        dispatch movetoworkspace special:sidebars,class:$CLASS ;
-        dispatch togglespecialworkspace sidebars ;
-        dispatch cyclenext"
+    if [[ $(hyprctl activewindow -j | jq -r ".class") != $CLASS ]]; then
+        hyprctl dispatch focuswindow class:$CLASS
+    else
+        # window is currently on the active workspace, so hide it by sending it to a
+        #   special workspace
+        hyprctl --batch "dispatch togglespecialworkspace sidebars ;
+            dispatch movetoworkspace special:sidebars,class:$CLASS ;
+            dispatch togglespecialworkspace sidebars ;
+            dispatch cyclenext"
+    fi
 else
     # window is located on another workspace, bring it to this one
     hyprctl dispatch movetoworkspace $current_workspace,"class:$CLASS"
-    if [[ "$side" == "right" ]]; then
-        hyprctl dispatch movewindow r
-    else
-        hyprctl dispatch movewindow l
-    fi
+    setup_window
 fi

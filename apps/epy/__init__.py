6f9b71b3977a502a5de69796736d127fab1bc6c5
Jeff <jdost@users.noreply.github.com>
2026-03-15 14:39:21 -0700

    [apps/epy] Install via pip
    AUR package is not working, switch to virtualenv based install

--- apps/epy/__init__.py
+++ apps/epy/__init__.py
@@ -1,12 +1,12 @@
-from cfgtools.files import DesktopEntry, XDGConfigFile, normalize
-from cfgtools.system.arch import AUR
+from cfgtools.files import DesktopEntry, UserBin, XDGConfigFile, normalize
+from cfgtools.system.python import VirtualEnv
 
 NAME = normalize(__name__)
 
-system_packages = {
-    AUR("epy-git")
-}
+virtualenv = VirtualEnv("epy", "epy-reader", "standard-imghdr")
+
 files = [
+    UserBin(virtualenv.location / "bin/epy", "epy"),
     XDGConfigFile(f"{NAME}/configuration.json", "epy/configuration.json"),
     DesktopEntry(f"{NAME}/epy.desktop"),
 ]

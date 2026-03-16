c9e3a448a6675a932a952eb38cffe21ffa727b57
Jeff <jdost@users.noreply.github.com>
2026-03-16 14:33:03 -0700

    [utils/networkmanager] Update setup
    No longer using the applet, instead want way to easily open the TUI
    editor.  Don't actively do much with networkmanager (just initial wifi
    activation) so mostly want something simple but un-intrusive.

--- utils/networkmanager/__init__.py
+++ utils/networkmanager/__init__.py
@@ -1,16 +1,11 @@
-from cfgtools.files import normalize
-from cfgtools.hooks import after
-from cfgtools.system.systemd import ensure_service, UserService
+from cfgtools.files import DesktopEntry, normalize
 from cfgtools.system.arch import Pacman
+from wayland.hyprland import HyprlandSettings
 
 NAME = normalize(__name__)
 
-system_packages={Pacman("network-manager-applet")}
-files=[
-    UserService(f"{NAME}/networkmanager-tray.service"),
+system_packages = {Pacman("networkmanager")}
+files = [
+    DesktopEntry(f"{NAME}/networkmanager.desktop"),
+    HyprlandSettings(f"{NAME}/hyprland.conf", "nmtui"),
 ]
-
-
-@after
-def start_nm_tray() -> None:
-    ensure_service("networkmanager-tray", user=True)
